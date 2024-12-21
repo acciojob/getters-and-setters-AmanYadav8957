aae9f14f9e0da3ee30561289080d94b31166663e
AmanYadav8957 <152906478+AmanYadav8957@users.noreply.github.com>
2024-12-21 23:36:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
-//complete this code
-class Person {}
+// Person class definition  
+class Person {  
+  constructor(name, age) {  
+    this._name = name; // Store name in a private property  
+    this._age = age;   // Store age in a private property  
+  }  
 
-class Student extends Person {}
+  // Getter for name  
+  get name() {  
+    return this._name;  
+  }  
 
-class Teacher extends Person {}
+  // Setter for age  
+  set age(value) {  
+    this._age = value; // Set the age value  
+  }  
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
+  // Getter for age (optional, if you want to access age)  
+  get age() {  
+    return this._age;  
+  }  
+}  
+
+// Student class that extends Person  
+class Student extends Person {  
+  constructor(name, age) {  
+    super(name, age); // Call the parent constructor  
+  }  
+
+  // Method for studying  
+  study() {  
+    console.log(`${this.name} is studying`);  
+  }  
+}  
+
+// Teacher class that extends Person  
+class Teacher extends Person {  
+  constructor(name, age) {  
+    super(name, age); // Call the parent constructor  
+  }  
+
+  // Method for teaching  
+  teach() {  
+    console.log(`${this.name} is teaching`);  
+  }  
+}  
+
+// Do not change the code below this line  
+window.Person = Person;  
+window.Student = Student;  
 window.Teacher = Teacher;
